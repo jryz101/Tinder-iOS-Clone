8ec9298382e2834c0083e8114e55b27be4cefa45
JerryTan <jryz101@gmail.com>
2020-01-23 15:14:09 +0800

    Initial Commit

---
+++ Tinder iOS (Clone)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder iOS (Clone)
+//
+//  Created by Jerry Tan on 23/01/2020.
+//  Copyright © 2020 Jerry Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
